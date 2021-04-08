627cc8648cea15b49c3856c639388e7e87fdeb0a
Kevin Romeiro <67075301+Kevin-028@users.noreply.github.com>
2021-04-08 10:36:54 -0300

    Modificacoes nas animacoes
    mudei as animacoes referentes ao menu de scroll, tanto para mobile quanto para desktop.

--- js/java.js
+++ js/java.js
@@ -3,6 +3,7 @@ var open_button 	= ".menu-open"; //Menu Open Button
 var close_button 	= ".menu-close"; //Menu Close Button
 var overlay 		= ".menu-overlay"; //Overlay
 var checkbox        = "#hamburger";
+var	link_menu		= ".link-menu";
 
 $(slide_wrp).hide().css( {"right": -$(slide_wrp).outerWidth()+'px'}).delay(50).queue(function(){$(slide_wrp).show()}); 
 
@@ -18,7 +19,7 @@ $(open_button).click(function(e){
 	$('#hamburger1').prop('checked', true);
 });
 
-$(close_button).click(function(e){
+$(close_button,).click(function(e){
 	e.preventDefault();
 	$(slide_wrp).css( {"right": -$(slide_wrp).outerWidth()+'px'});
 	setTimeout(function(){
@@ -39,4 +40,38 @@ $(document).on('click', function(e) {
 		$('#hamburger1').prop('checked', false);
 
 	}
+});
+// Desktop!!
+// funcao que define a possicao do scroll do click da ancora referente a id da secao
+$('.btn-menu ul li a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100 // define a altura que meu scroll vai ficar do top da pagina
+	}, 500);
+
+});
+
+
+// MOBILE!!
+// funcao que define a possicao do scroll do click da ancora referente a id da secao
+$('.side-menu-wrapper ul li a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 120 // define a altura que meu scroll vai ficar do top da pagina
+	}, 500);
+
+	// fecha o menu do mobile
+	$(slide_wrp).css( {"right": -$(slide_wrp).outerWidth()+'px'});
+	setTimeout(function(){
+		$(slide_wrp).removeClass('active');
+	},50);
+	$(overlay).css({"opacity":"0", "width":"0"});
+	$('#hamburger').prop('checked', false);
+	$('#hamburger1').prop('checked', false);
 });
